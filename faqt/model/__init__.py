c80fa7aeb5b99a8d82e14211ac3f04fbdaa9a3e9
Suzin You <suzinyou.sy@gmail.com>
2022-09-05 22:04:18 +0900

    [PNC-542] implement BERT based scorer

--- faqt/model/__init__.py
+++ faqt/model/__init__.py
@@ -2,8 +2,8 @@ from faqt.model.faq_matching.keyed_vectors_scoring import (
     KeyedVectorsScorer,
     StepwiseKeyedVectorsScorer,
 )
+from faqt.model.faq_matching.bert import QuestionAnswerBERTScorer
 from faqt.model.urgency_detection.keyword_rule_matching_model import (
     KeywordRule,
     evaluate_keyword_rules,
 )
-
