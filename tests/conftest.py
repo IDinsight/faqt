1997bd4dbca28ac224a954cb3aa0c4d0069e9ef9
Suzin You <suzinyou.sy@gmail.com>
2022-09-08 23:14:45 +0900

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+import os
+from pathlib import Path
+
 import pytest
-from tests.utils import load_wv_pretrained_bin
 from hunspell import Hunspell
+from tests.utils import download_huggingface_model_from_s3, load_wv_pretrained_bin
 
 
 @pytest.fixture(scope="session")
@@ -15,3 +18,19 @@ def w2v_model():
 def hunspell():
     hunspell = Hunspell()
     return hunspell
+
+
+@pytest.fixture(scope="session")
+def bert_model_path():
+    folder = "sequence_classification_models"
+    model_folder = (
+        "huggingface-pytorch-training-2022-07-30-15-57-13" "-503__output__model"
+    )
+
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        bucket = os.getenv("WORD2VEC_BINARY_BUCKET")
+        full_path = download_huggingface_model_from_s3(bucket, folder, model_folder)
+    else:
+        full_path = Path(__file__).parent / "data" / folder / model_folder
+
+    return full_path
